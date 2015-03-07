164182408d288ca2a6863e1746fe132ce04f76f2
aleontiev <alonetiev@gmail.com>
2015-03-07 00:38:30 -0500

    add Makefile for tests and demo server, viewset changes

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
-from distutils.core import setup
+from setuptools import setup
+from pip.req import parse_requirements
 import codecs
+import os
+import re
 
 PACKAGE_NAME = 'dynamic_rest'
 BASE_DIR = os.path.dirname(__file__) or "."
 VERSION = re.search('__version__ = "([^"]+)"',
                     codecs.open("%s/%s/__init__.py" % (BASE_DIR, PACKAGE_NAME), encoding='utf-8').read()).group(1)
+
+install_requirements = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_requirements]
+
 setup(
     author="Anthony Leontiev",
     author_email="ant@altschool.com",
     description="Dynamic extensions for Django REST Framework",
+    install_requires=reqs,
     long_description=open("README.md").read(),
     name=PACKAGE_NAME,
     packages=[
         PACKAGE_NAME,
         "tests",
     ],
+    scripts=['manage.py'],
     url="http://github.com/AltSchool/dynamic-rest",
     version=VERSION,
     test_suite='runtests.runtests'
--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,15 @@
+import os
 SECRET_KEY = 'test'
+INSTALL_DIR = '/usr/local/dynamic-rest/'
+STATIC_URL = '/static/'
+STATIC_ROOT = INSTALL_DIR + 'www/static'
+
+DEBUG = True
 
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': 'db.sqlite3',
+    'NAME': INSTALL_DIR + 'db.sqlite3',
     'USER': '',
     'PASSWORD': '',
     'HOST': '',
@@ -14,7 +20,11 @@ DATABASES = {
 MIDDLEWARE_CLASSES = ()
 
 INSTALLED_APPS = (
+  'rest_framework',
+  'django.contrib.staticfiles',
   'tests',
 )
 
 ROOT_URLCONF = 'tests.urls'
+
+BASE_DIR = os.path.dirname(__file__)
