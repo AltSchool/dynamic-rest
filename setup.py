4ac7ef17442a030ef2c22967f28207982c43c191
aleontiev <alonetiev@gmail.com>
2015-04-06 21:10:56 -0400

    remove install_requires section

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from pip.req import parse_requirements
 import codecs
 import os
 import re
@@ -9,14 +8,10 @@ BASE_DIR = os.path.dirname(__file__) or "."
 VERSION = re.search('__version__ = "([^"]+)"',
                     codecs.open("%s/%s/__init__.py" % (BASE_DIR, PACKAGE_NAME), encoding='utf-8').read()).group(1)
 
-install_requirements = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_requirements]
-
 setup(
     author="Anthony Leontiev",
     author_email="ant@altschool.com",
     description="Dynamic extensions for Django REST Framework",
-    install_requires=reqs,
     long_description=open("README.md").read(),
     name=PACKAGE_NAME,
     packages=[
