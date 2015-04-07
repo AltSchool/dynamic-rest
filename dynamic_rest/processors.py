7312e82bda70a250175d17044b8a1212e248c12f
aleontiev <alonetiev@gmail.com>
2015-04-07 11:43:57 -0400

    add TaggedDict for sideloading

--- dynamic_rest/processors.py
+++ dynamic_rest/processors.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from rest_framework.serializers import ListSerializer
+from dynamic_rest.wrappers import TaggedDict
 
 class SideloadingProcessor(object):
   def __init__(self, serializer, data):
@@ -7,7 +8,6 @@ class SideloadingProcessor(object):
       serializer = serializer.child
 
     self.data = {}
-    self.sideload = serializer.sideload
     self.seen = defaultdict(set)
     self.plural_name = serializer.get_plural_name()
     self.name = serializer.get_name()
@@ -23,13 +23,12 @@ class SideloadingProcessor(object):
     if isinstance(data, list):
       return self.is_dynamic(data[0]) if len(data) else False
     if isinstance(data, dict):
-      return '_pk' in data and '_name' in data
+      return isinstance(data, TaggedDict)
     return False
 
   def process(self, obj, parent=None, parent_key=None, depth=0):
     """
-    Recursively traverse the response data, remove identifying tags added by serializers
-    and sideload if necessary.
+    Recursively traverse the response data to sideload.
     """
     if isinstance(obj, list):
       for key, o in enumerate(obj):
@@ -37,8 +36,8 @@ class SideloadingProcessor(object):
         self.process(o, parent=obj, parent_key=key, depth=depth)
     elif isinstance(obj, dict):
       if self.is_dynamic(obj):
-        name = obj.pop('_name')
-        pk = obj.pop('_pk')
+        name = obj.serializer.get_plural_name()
+        pk = obj.instance.pk
 
         # recursively check all fields
         for key, o in obj.iteritems():
@@ -46,12 +45,7 @@ class SideloadingProcessor(object):
             # lists or dicts indicate a relation
             self.process(o, parent=obj, parent_key=key, depth=depth + 1)
 
-        # if sideloading is off, stop here
-        if not self.sideload:
-          return
-
         # sideloading
-
         seen = True
         # if this object has not yet been seen
         if not pk in self.seen[name]:
