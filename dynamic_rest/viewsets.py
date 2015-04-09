b561ea5cf2afe09f4131e99eb547765628f85f1d
Ryo Chijiiwa <ryo@altschool.com>
2015-04-09 13:08:40 -0700

    add filter support

--- dynamic_rest/viewsets.py
+++ dynamic_rest/viewsets.py
@@ -1,17 +1,47 @@
 from django.conf import settings
-from django.db.models import Prefetch, ManyToManyField
+from django.db.models import Q, Prefetch, ManyToManyField
 from django.db.models.related import RelatedObject
+from django.http import QueryDict
 
 from dynamic_rest.fields import DynamicRelationField
 from dynamic_rest.pagination import DynamicPageNumberPagination
 from dynamic_rest.metadata import DynamicMetadata
+from dynamic_rest.datastructures import TreeMap
 
 from rest_framework import viewsets, response, exceptions, serializers
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 
 dynamic_settings = getattr(settings, 'DYNAMIC_REST', {})
-class DynamicModelViewSet(viewsets.ModelViewSet):
 
+class QueryParams(QueryDict):
+  """
+  Extension of Django's QueryDict. Instantiated from a DRF Request
+  object, and returns a mutable QueryDict subclass.
+  Also adds methods that might be useful for our usecase.
+  """
+
+  def __init__(self, query_params, *args, **kwargs):
+    query_string = getattr(query_params, 'urlencode', lambda: '')() 
+    kwargs['mutable'] = True 
+    super(QueryParams, self).__init__(query_string, *args, **kwargs)
+
+  def add(self, key, value):
+    """
+    Method to accept a list of values and append to flat list.
+    QueryDict.appendlist(), if given a list, will append the list, 
+    which creates nested lists. In most cases, we want to be able
+    to pass in a list (for convenience) but have it appended into
+    a flattened list.
+    TODO: Possibly throw an error if add() is used on a non-list param.
+    """
+    if isinstance(value, list):
+      for val in value:
+        self.appendlist(key, val)
+    else:
+      self.appendlist(key, value)
+
+
+class WithDynamicViewSetMixin(object):
   """A viewset that can support dynamic API features.
 
   Attributes:
@@ -22,65 +52,41 @@ class DynamicModelViewSet(viewsets.ModelViewSet):
 
   INCLUDE = 'include[]'
   EXCLUDE = 'exclude[]'
+  FILTER = 'filter{}'
   PAGE = dynamic_settings.get('PAGE_QUERY_PARAM', 'page')
   PER_PAGE = dynamic_settings.get('PAGE_SIZE_QUERY_PARAM', 'per_page')
 
-  # TODO: add support for `filter{}`, `sort{}`, `page`, and `per_page`
+  # TODO: add support for `sort{}`, `page`, and `per_page`
   pagination_class = DynamicPageNumberPagination
   metadata_class = DynamicMetadata
   renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
-  features = (INCLUDE, EXCLUDE, PAGE, PER_PAGE)
+  features = (INCLUDE, EXCLUDE, FILTER, PAGE, PER_PAGE)
   sideload = True
   meta = None
 
-  def get_queryset(self, serializer=None):
-    """Returns a queryset for this request.
+  VALID_OPERATORS = (
+    'in',
+    'any',
+    'all',
+    'like',
+    'range',
+    'gt',
+    'lt',
+    'gte',
+    'lte',
+    'isnull',
+    None,
+    )
 
-    Handles nested prefetching of related data and deferring fields
-    at the queryset level.
-
-    Arguments:
-      serializer: An optional serializer to use a base for the queryset.
-        If no serializer is passed, the `get_serializer` method will be used
-        to initialize the base serializer for the viewset.
+  def initialize_request(self, request, *args, **kargs):
+    """ 
+    Override DRF initialize_request() method to swap request.GET
+    (which is aliased by request.QUERY_PARAMS) with a mutable instance
+    of QueryParams.
     """
-    if serializer:
-      queryset = serializer.Meta.model.objects.all()
-    else:
-      serializer = self.get_serializer()
-      queryset = getattr(self, 'queryset', serializer.Meta.model.objects.all())
-
-    prefetch_related = []
-    only = set()
-    use_only = True
-    model = serializer.Meta.model
-
-    for name, field in serializer.get_fields().iteritems():
-      if isinstance(field, DynamicRelationField):
-        field = field.serializer
-      if isinstance(field, serializers.ListSerializer):
-        field = field.child
-
-      source = field.source or name
-      source0 = source.split('.')[0]
-      remote = False
-
-      if isinstance(field, serializers.ModelSerializer):
-        model_field = model._meta.get_field_by_name(source0)[0]
-        remote = isinstance(model_field, (ManyToManyField, RelatedObject))
-        if not getattr(field, 'id_only', lambda: False)() or remote:
-          prefetch_related.append(Prefetch(source, queryset=self.get_queryset(field)))
-
-      if use_only:
-        if source == '*':
-          use_only = False
-        elif not remote:
-          # TODO: optimize for nested sources
-          only.add(source0)
-
-    if use_only:
-      queryset = queryset.only(*only)
-    return queryset.prefetch_related(*prefetch_related)
+    request.GET = QueryParams(request.GET)
+    return super(WithDynamicViewSetMixin, self).initialize_request(
+        request, *args, **kargs)
 
   def get_request_feature(self, name):
     """Parses the request for a particular feature.
@@ -100,6 +106,189 @@ class DynamicModelViewSet(viewsets.ModelViewSet):
     else:
       # single-type
       return self.request.QUERY_PARAMS.get(name) if name in self.features else None
+
+  def _extract_filters_map(self):
+    """
+    Extract filter params, return as dict
+    NOTE: Supports 'filters{}' and 'filter{}' due to an implementation error.
+          In the future, 'filters' will be deprecated.
+    """
+    params = self.request.query_params.lists()
+    filters_map = {}
+    for name, value in params:
+      if name.startswith('filters{') and name.endswith('}'):
+        name = name[8:-1]
+      elif name.startswith('filter{') and name.endswith('}'):
+        name = name[7:-1]
+      else:
+        continue
+      filters_map[name] = value
+
+    return filters_map
+
+  def _extract_filters(self, **kwargs):
+    """ 
+    Convert 'filters' query params into a dict that can be passed
+    to Q. Returns a dict with two fields, 'include' and 'exclude',
+    which can be used like:
+
+      result = self._extract_filters()
+      q = Q(**result['include'] & ~Q(**result['exclude'])
+
+    """
+
+    filters_map = kwargs.get('filters_map') or self._extract_filters_map()
+
+    prefix = 'filters{'
+    offset = len(prefix)
+    out = TreeMap() 
+
+    for spec, value in filters_map.iteritems():
+
+      # Inclusion or exclusion?
+      if spec[0]=='-':
+        spec= spec[1:]
+        inex = '_exclude'
+      else:
+        inex = '_include'
+
+      # for relational filters, separate out relation path part 
+      if '|' in spec:
+        rel, spec = spec.split('|')  
+        rel = rel.split('.')
+      else:
+        rel = None
+
+      parts = spec.split('.')
+
+      # if dot-delimited, assume last part is the operator, otherwise
+      # assume whole thing is a field name (with 'eq' implied).
+      field = '__'.join(parts[:-1]) if len(parts)>1 else parts[0] 
+
+      # Assume last part of a dot-delimited field spec is an operator.
+      # Note, however, that 'foo.bar' is a valid field spec with an 'eq'
+      # implied as operator. This will be resolved below.
+      operator = parts[-1] if len(parts) > 1 and parts[-1]!='eq' else None
+
+      # All operators except 'range' and 'in' should have one value
+      if operator == 'range':
+        value = value[:2]
+      elif operator == 'in':
+        # no-op: i.e. accept `value` as an arbitrarily long list
+        pass
+      elif operator in self.VALID_OPERATORS:
+        value = value[0]
+      else:
+        # Unknown operator, we'll treat it like a field 
+        # e.g: filter{foo.bar}=baz
+        field += '__' + operator
+        operator = None
+        value = value[0]
+
+      param = field
+      if operator:
+        param += '__'+operator 
+
+      path = rel if rel else []
+      path.extend([inex, param])
+      out.insert(path, value)
+
+    return out 
+
+
+  def _filters_to_query(self, includes, excludes, q=None): 
+    """
+    Construct Django Query object from request.
+    Arguments are dictionaries, which will be passed to Q() as kwargs.
+
+    e.g.
+        includes = { 'foo' : 'bar', 'baz__in' : [1, 2] }
+      produces:
+        Q(foo='bar', baz__in=[1, 2])
+
+    Arguments:
+      includes: dictionary of inclusion filters
+      excludes: dictionary of inclusion filters
+
+    Returns:
+      Q() instance or None if no inclusion or exclusion filters were specified
+    """
+
+    q = q or Q() 
+
+    if not includes and not excludes:
+      return None
+
+    if includes:
+      q &= Q(**includes)
+    if excludes:
+      for k,v in excludes.iteritems():
+        q &= ~Q(**{k:v})
+    return q
+
+
+  def get_queryset(self, serializer=None, filters=None):
+    """Returns a queryset for this request.
+
+    Handles nested prefetching of related data and deferring fields
+    at the queryset level.
+
+    Arguments:
+      serializer: An optional serializer to use a base for the queryset.
+        If no serializer is passed, the `get_serializer` method will be used
+        to initialize the base serializer for the viewset.
+    """
+    if serializer:
+      queryset = serializer.Meta.model.objects
+    else:
+      serializer = self.get_serializer()
+      queryset = getattr(self, 'queryset', serializer.Meta.model.objects.all())
+
+    prefetch_related = []
+    only = set()
+    use_only = True
+    model = serializer.Meta.model
+
+    if filters == None:
+      filters = self._extract_filters()  
+    q = self._filters_to_query(
+        includes=filters.get('_include'), excludes=filters.get('_exclude'))
+
+    for name, field in serializer.get_fields().iteritems(): 
+      if isinstance(field, DynamicRelationField):
+        field = field.serializer
+      if isinstance(field, serializers.ListSerializer):
+        field = field.child
+
+      source = field.source or name
+      source0 = source.split('.')[0]
+      remote = False
+
+      if isinstance(field, serializers.ModelSerializer):
+        model_field = model._meta.get_field_by_name(source0)[0]
+        remote = isinstance(model_field, (ManyToManyField, RelatedObject))
+        id_only = getattr(field, 'id_only', lambda: False)()
+        if not id_only or remote:
+          prefetch_qs = self.get_queryset(
+              serializer=field, filters=filters.get(name,{}))
+          prefetch_related.append(Prefetch(source, queryset=prefetch_qs))
+
+      if use_only:
+        if source == '*':
+          use_only = False
+        elif not remote:
+          # TODO: optimize for nested sources
+          only.add(source0)
+
+    if getattr(serializer, 'id_only', lambda: False)():
+      only = serializer.get_id_fields()
+      use_only = True
+
+    if use_only:
+      queryset = queryset.only(*only)
+    if q:
+      queryset = queryset.filter(q)
+    return queryset.prefetch_related(*prefetch_related)
 
   def get_request_fields(self):
     """Parses the `include[]` and `exclude[]` features.
@@ -127,7 +316,7 @@ class DynamicModelViewSet(viewsets.ModelViewSet):
           last = i == num_segments - 1
           if segment:
             if last:
-              current_fields[segment] = include
+              current_fields[segment] = include 
             else:
               if not segment in current_fields:
                 current_fields[segment] = {}
@@ -140,7 +329,7 @@ class DynamicModelViewSet(viewsets.ModelViewSet):
     return request_fields
 
   def get_serializer_context(self):
-    context = super(DynamicModelViewSet, self).get_serializer_context()
+    context = super(WithDynamicViewSetMixin, self).get_serializer_context()
     context['request_fields'] = self.get_request_fields()
     context['do_sideload'] = self.sideload
     return context
@@ -152,5 +341,9 @@ class DynamicModelViewSet(viewsets.ModelViewSet):
         self.PER_PAGE in self.request.QUERY_PARAMS:
         # remove per_page if it is disabled
         self.request.QUERY_PARAMS[self.PER_PAGE] = None
-      return super(DynamicModelViewSet, self).paginate_queryset(*args, **kwargs)
+      return super(WithDynamicViewSetMixin, self).paginate_queryset(*args, **kwargs)
     return None
+
+
+class DynamicModelViewSet(WithDynamicViewSetMixin, viewsets.ModelViewSet):
+  pass
