58096380ed77c585d9ccea07c1548d53cec21723
aleontiev <alonetiev@gmail.com>
2016-01-27 13:57:36 -0800

    add screenshots branch

--- benchmarks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url, include
-from .drest import router as drest_router
-from .drf import router as drf_router
-
-urlpatterns = patterns(
-    '',
-    url(r'^', include(drf_router.urls)),
-    url(r'^', include(drest_router.urls)),
-)
--- constants.py
+++
@@ -1,8 +0,0 @@
-APP_NAME = "dynamic_rest"
-AUTHOR = "Anthony Leontiev"
-AUTHOR_EMAIL = "ant@altschool.com, ryo@altschool.com"
-DESCRIPTION = "Adds Dynamic API support to Django REST Framework."
-REPO_NAME = "dynamic-rest"
-PROJECT_NAME = "Dynamic REST"
-ORG_NAME = "AltSchool"
-VERSION = "1.3.9"
