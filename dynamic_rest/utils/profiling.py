2755238cdb42cdcf2ebe4c1551d3e9bb02e8a860
Ryo Chijiiwa <ryo@altschool.com>
2016-05-25 16:06:02 -0700

    custom profiler

--- dynamic_rest/utils/profiling.py
+++ dynamic_rest/utils/profiling.py
@@ -1,7 +1,10 @@
 import cProfile
+import gevent
+import json
 import pstats
 import resource
 import StringIO
+import sys
 
 
 class Profiling(object):
@@ -56,6 +59,106 @@ class CPUTimer(object):
         used = get_cpu_usage() - self.start_cpu
         print("CPU Usage: %.4f secs" % used)
 
+
+class Node(json.JSONEncoder):
+    def __init__(self, func, file_loc, starting_cpu):
+        self.func = func
+        self.file_loc = file_loc
+        self.starting_cpu = starting_cpu
+        self.ending_cpu = None
+        self.dur = None
+        self.children = []
+
+    def end(self, ending_cpu):
+        self.dur = ending_cpu - self.starting_cpu
+        self.ending_cpu = ending_cpu
+
+    def add(self, node):
+        assert isinstance(node, Node)
+        assert self.dur is None  # frame should not have ended
+        self.children.append(node)
+
+    def to_dict(self):
+        children = []
+        for child in self.children:
+            children.append(child.to_dict())
+        name = '/'.join(self.file_loc.split('/')[-3:]) if self.file_loc else ''
+        name = "%s %s" % (self.func, name)
+        return {
+            'func': self.func,
+            'file_loc': self.file_loc,
+            'name': name,
+            'value': round(self.dur, 5) if self.dur else None,
+            'starting_cpu': self.starting_cpu,
+            'ending_cpu': self.ending_cpu,
+            'dur': self.dur,
+            'children': children
+        }
+
+    @classmethod
+    def from_frame(cls, frame):
+        return cls(
+            func=frame[1],
+            file_loc=frame[3],
+            starting_cpu=frame[2]
+        )
+
+class Profiler(object):
+    def __init__(self, outfile_path, greenlet=None):
+        self.ref = None
+        self.root = None
+        self.outfile_path = outfile_path
+        self.original_profiler = sys.getprofile()
+        self.frames = []
+        self.greenlet_id = id(greenlet) if greenlet else None
+
+    def profiler(self, frame, event, arg):
+        if self.greenlet_id and id(gevent.getcurrent()) != self.greenlet_id:
+            return
+        if event != 'call' and event != 'return':
+            return
+        self.frames.append((
+            event,
+            frame.f_code.co_name,
+            get_cpu_usage(),
+            "%s:%s" % (frame.f_code.co_filename, frame.f_lineno),
+        ))
+
+    def build_tree(self):
+        stack = []
+
+        for i, frame in enumerate(self.frames):
+            event = frame[0]
+            if event == 'call':
+                parent = stack[-1] if stack else self.root
+                node = Node.from_frame(frame)
+                parent.add(node)
+                stack.append(node)
+            elif event == 'return':
+                if not stack:  # no matching 'call' event logged
+                    continue
+                node = stack.pop()
+                if node.func != frame[1]:
+                    import pdb
+                    pdb.set_trace()
+                assert node.func == frame[1]
+                node.end(frame[2])
+
+        return self.root
+
+    def start(self):
+        self.root = Node('root', None, get_cpu_usage())
+        sys.setprofile(self.profiler)
+
+    def end(self):
+        sys.setprofile(self.original_profiler)
+        end = get_cpu_usage()
+        root = self.build_tree()
+        self.root.end(end)
+        if self.outfile_path:
+            fp = open(self.outfile_path, 'w')
+            fp.write(json.dumps(root.to_dict()))
+            fp.close()
 
 def run_test():
     from tests.serializers import UserSerializer
