5dcf1abc00318eeac5f89de08c35be903295760a
aleontiev <alonetiev@gmail.com>
2017-03-13 13:28:02 -0700

    remove dependency_links (unused)

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ SCRIPTS = ['manage.py']
 setup(
     description=DESCRIPTION,
     include_package_data=True,
-    dependency_links=open('dependency_links.txt').readlines(),
     install_requires=open('install_requires.txt').readlines(),
     long_description=open('README.rst').read(),
     name=NAME,
