bf3283a9b0efe96f0f0ae2997bbe2dcc695a6850
Alex Tzannes <alex.tzannes@altschool.com>
2017-04-12 12:08:41 +0300

    Changes the location of db.sqlite3 for the test
    On linux based systems, the previous default location for
    db.sqlite3 needs super-user privileges to write to, which
    seems unnecessary. Additionally, the error message is not
    very informative ('unable to open database file')

--- tests/settings.py
+++ tests/settings.py
@@ -21,7 +21,7 @@ else:
     # local sqlite database file
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': INSTALL_DIR + 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
