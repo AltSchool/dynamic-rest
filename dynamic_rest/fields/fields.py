8ae537bb8d492bb34cdca6600393a53bf3008358
Raja Gangopadhya <rajag@altschool.com>
2017-06-28 10:37:53 -0700

    Make error handling more robust, if debug isnt an attribute

--- dynamic_rest/fields/fields.py
+++ dynamic_rest/fields/fields.py
@@ -296,7 +296,7 @@ class DynamicRelationField(WithRelationalFieldMixin, DynamicField):
             return serializer.to_representation(related)
         except Exception as e:
             # Provide more context to help debug these cases
-            if serializer.debug:
+            if getattr(serializer, 'debug', False):
                 import traceback
                 traceback.print_exc()
             raise Exception(
