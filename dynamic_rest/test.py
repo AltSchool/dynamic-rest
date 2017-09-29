ef351755020dc88566fccf22ed99e0ebabbeedcd
aleontiev <alonetiev@gmail.com>
2017-09-29 11:02:14 -0700

    import from compat

--- dynamic_rest/test.py
+++ dynamic_rest/test.py
@@ -4,7 +4,7 @@ from model_mommy import mommy
 from rest_framework.fields import empty
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
-from django.urls import resolve
+from .compat import resolve
 from dynamic_rest.meta import Meta
 
 
