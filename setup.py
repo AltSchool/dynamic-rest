39045688f508254f1937f58bc900623e7adca227
Savinay Nangalia <savinay.ng@gmail.com>
2020-05-29 11:06:50 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 NAME = 'dynamic-rest'
 DESCRIPTION = 'Adds Dynamic API support to Django REST Framework.'
 URL = 'http://github.com/AltSchool/dynamic-rest'
-VERSION = '1.9.7'
+VERSION = '1.9.8'
 SCRIPTS = ['manage.py']
 
 setup(
