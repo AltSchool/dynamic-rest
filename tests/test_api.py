4a9200e3c9540d54ee4b81e5be643d234b059c90
Anthony Leontiev <alonetiev@gmail.com>
2020-06-23 15:48:32 -0700

    add fix for sort by relation (#304)

--- tests/test_api.py
+++ tests/test_api.py
@@ -925,6 +925,15 @@ class TestUsersAPI(APITestCase):
             [row['location'] for row in data['users']]
         )
 
+    def test_sort_relation_field_many(self):
+        url = '/locations/?sort[]=friendly_cats.name'
+        response = self.client.get(url)
+        self.assertEquals(200, response.status_code)
+        data = json.loads(response.content.decode('utf-8'))
+        ids = [row['id'] for row in data['locations']]
+        # no duplicates
+        self.assertEquals(len(ids), len(set(ids)))
+
 
 @override_settings(
     DYNAMIC_REST={
