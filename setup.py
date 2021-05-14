4cbac1f1cda6b292cd0fa7c620a1377ee31fba0d
mattiagiupponi <51856725+mattiagiupponi@users.noreply.github.com>
2021-05-14 16:16:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 NAME = 'dynamic-rest'
 DESCRIPTION = 'Adds Dynamic API support to Django REST Framework.'
 URL = 'http://github.com/AltSchool/dynamic-rest'
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 SCRIPTS = ['manage.py']
 
 setup(
