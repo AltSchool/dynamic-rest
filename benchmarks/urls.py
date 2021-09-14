103740c49306a386fc652971cbd8a399dd8132c8
Ernesto F. González <ernestogonzalezz@yahoo.com>
2021-09-14 22:13:16 +0100

    Remove deprecated `patterns` method usage

--- benchmarks/urls.py
+++ benchmarks/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, path
 
 from .drest import router as drest_router
 from .drf import router as drf_router
 
-urlpatterns = patterns(
-    '',
-    url(r'^', include(drf_router.urls)),
-    url(r'^', include(drest_router.urls)),
-)
+urlpatterns = [
+    path(r'^', include(drf_router.urls)),
+    path(r'^', include(drest_router.urls)),
+]
