3b9c5a36870285959363a692b98d335048c0fd05
BillSchumacher <34168009+BillSchumacher@users.noreply.github.com>
2023-09-16 00:22:10 -0500

    Use new cases.

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,9 +1,12 @@
 """Test cases for the API."""
-from django.test import TestCase
+from django.test import (  # noqa pylint: disable=unused-import
+    TestCase,
+    TransactionTestCase,
+)
 from rest_framework.test import APITestCase
 
 
-class ResetTestCase(TestCase):
+class ResetTestCase(TransactionTestCase):
     """Reset API test case."""
 
     reset_sequences = True
