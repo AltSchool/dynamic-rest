e9433953529822628c20ae03c19dc803ed787482
Niji <mathereall@gmail.com>
2023-11-26 14:26:33 -0600

    WIP for publish on pypi change versioning to YYYY.MM[-TAGNUM] format and wrapping module into src directory

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import find_packages, setup
-from src.dynamic_rest import __version__
 
 NAME = 'dynamic-rest'
 DESCRIPTION = 'Dynamic API support to Django REST Framework.'
 URL = 'https://github.com/cesar-benjamin/drf-dynamic-rest'
-VERSION = __version__
+VERSION = "2023.11-alpha7"
 SCRIPTS = ['manage.py']
 
 setup(
     description=DESCRIPTION,
-    include_package_data=True,
     install_requires=open('install_requires.txt').readlines(),
     long_description=open('README.rst').read(),
     name=NAME,
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     scripts=SCRIPTS,
     url=URL,
     version=VERSION,
@@ -22,7 +21,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
