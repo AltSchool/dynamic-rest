a4508507b64e9d39613dcb0da6ef49c8217a7036
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2024-05-31 00:39:42 +0530

    chore: Support Django 5.0

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import find_packages, setup
 
-NAME = 'dynamic-rest'
-DESCRIPTION = 'Dynamic API support to Django REST Framework.'
-URL = 'http://github.com/AltSchool/dynamic-rest'
-VERSION = '2.3.0'
-SCRIPTS = ['manage.py']
+NAME = "dynamic-rest"
+DESCRIPTION = "Dynamic API support to Django REST Framework."
+URL = "http://github.com/AltSchool/dynamic-rest"
+VERSION = "2.4.0"
+SCRIPTS = ["manage.py"]
 
 setup(
     description=DESCRIPTION,
     include_package_data=True,
-    install_requires=open('install_requires.txt').readlines(),
-    long_description=open('README.rst').read(),
+    install_requires=open("install_requires.txt").readlines(),
+    long_description=open("README.rst").read(),
     name=NAME,
     packages=find_packages(),
     scripts=SCRIPTS,
     url=URL,
     version=VERSION,
     classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
